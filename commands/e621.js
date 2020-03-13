7f47e5cd33029751825235887ba1edd18be3977c
Josh Conner <jconner05@fastmail.com>
2020-03-12 23:27:19 -0700

    added global tag to repalce method

--- commands/e621.js
+++ commands/e621.js
@@ -4,10 +4,9 @@ let client = null;
 
 module.exports = (search, msg, parent_client) => {
   client = parent_client;
-  // let mod_search = search.append("-cub")
   if(msg.channel.id != "598847956996718628" && msg.channel.id != "638739285343993886") {
-    let search_string = search.toString().replace(',', ' ').concat(' -female -cub');
-    console.log(search_string.replace(/ /g, '+'));
+    let search_string = search.toString().replace(/,/g, ' ').concat(' -female -cub');
+    // console.log(search_string.replace(/ /g, '+'));
     let url = `https://e621.net/posts.json?tags=${search_string.replace(/ /g, '+')}&limit=150`;
     // console.log(url);
     fetch(url, {
@@ -23,9 +22,8 @@ module.exports = (search, msg, parent_client) => {
     .then((posts) => {
     let embed = null;
     //  console.log(posts);
-    //  let result = posts.map(a => a.file);
     let post = posts[Math.floor(Math.random() * posts.length)];
-    console.log(`URL: ${post.file.url}`);
+    // console.log(`URL: ${post.file.url}`);
     if(post.file.url.substring(post.file.url.length - 4) != `webm`) {
       embed = embedBuilder(`e621`, msg.author, `RANDOM`, ``, post.file.url, [{"name": "Link: ", "value": `https://e621.net/posts/${post.id}`}]);
     }
