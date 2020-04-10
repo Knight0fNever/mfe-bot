545ba7cdd4173f9b9f0338de3d2662fd2ca96aa9
Josh Conner <jconner05@fastmail.com>
2020-04-10 05:49:56 -0700

    added new commands to help

--- commands/help.js
+++ commands/help.js
@@ -22,6 +22,11 @@ module.exports = (msg, command) => {
         "name": "e621",
         "value": "`mfe! e621 <search>`",
         "inline": true
+      },
+      {
+        "name": "suggest - sends a suggestion for the bot to the developer",
+        "value": "`mfe! suggest <suggestion>`",
+        "inline": true
       }
     ]
 
@@ -38,7 +43,7 @@ module.exports = (msg, command) => {
       .setTitle("MFE-Bot Actions")
       .setColor("RANDOM")
       .setAuthor(msg.author.tag, msg.author.avatarURL())
-      .setDescription("bellyrub\n boop\n cuddle\n flop\n hug\n kiss\n lick\n nap\n nuzzle\n pat\n poke\n pounce\n slap\n sniff\n spray\n wag\n whosagoodboy\n fuck\n pant\n eatPant\n suck")
+      .setDescription("bellyrub\n boop\n cuddle\n flop\n hug\n kiss\n lick\n nap\n nuzzle\n pat\n poke\n pounce\n slap\n sniff\n spray\n wag\n whosagoodboy\n fuck\n pant\n eatPant\n suck\n rub\n tie")
 
     msg.channel.send(response);
   }
